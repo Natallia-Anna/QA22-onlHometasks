3b0f5985e1b4bed4c15427723fd68e04e72d725d
Natallia-Anna <moyseencko-natalya@yandex.ru>
2023-06-28 20:23:48 +0300

    Solved

--- src/HelloUser.java
+++ src/HelloUser.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class HelloUser {
+import java.util.Scanner;
+
+public class HelloUser
+{
+    public static void main (String [] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        System.out.println("Enter name: ");
+        name = scanner.next();
+        System.out.println("Hello " + name);
+    }
+
 }
